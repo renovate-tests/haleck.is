2671f18947524e878b8240ad6ccbe082b93dd9dd
Artjoms <artjoms.haleckis@gmail.com>
2019-11-06 20:48:59 +0200

    Logo refactoring

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,9 +3,8 @@ import React from "react"
 const Logo = () => {
   return (
     <div class="logo_container">
-      <div class="logo_outer">
-        <div class="logo_inner"></div>
-      </div>
+      <div class="logo_outer"></div>
+      <div class="logo_inner"></div>
     </div>
   )
 }
