1a71c3ec3ad9540a09126bf9d05d5a086ed5ce02
Artjoms <artjoms.haleckis@gmail.com>
2019-11-09 12:15:46 +0200

    Global config updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,8 +65,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Artjoms Haleckis - Blog`,
+        short_name: `halec.is`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
